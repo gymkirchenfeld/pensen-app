d719c4ab27cfea45e050349028f37994451360a5
Stefan Rothe <stefan.rothe@gymkirchenfeld.ch>
2023-12-27 10:16:58 +0100

    Improved rules.

--- src/utils/rules.js
+++ src/utils/rules.js
@@ -1,15 +1,24 @@
+// https://stackoverflow.com/questions/175739, adapted
+function isNumeric(str) {
+  return (
+    (typeof str === 'string' && str === '') ||
+    (!isNaN(str) && !isNaN(parseFloat(str)))
+  );
+}
+
 export default {
   nonNegativeNumber: [
-    (v) => Boolean(v) || 'Das Feld muss eine Zahl enthalten',
-    (v) => 0 <= v || 'Das Feld muss eine nicht-negative Zahl enthalten.',
+    (v) => isNumeric(v) || 'Das Feld muss eine Zahl enthalten',
+    (v) =>
+      0 <= parseFloat(v) || 'Das Feld muss eine nicht-negative Zahl enthalten.',
   ],
   week: [
-    (v) => Boolean(v) || 'Das Feld muss eine Zahl enthalten',
+    (v) => isNumeric(v) || 'Das Feld muss eine Zahl enthalten',
     (v) =>
       (0 < v && v < 54) || 'Das Feld muss die Anzahl Schulwochen enthalten.',
   ],
   year: [
-    (v) => Boolean(v) || 'Das Feld muss eine Zahl enthalten',
+    (v) => isNumeric(v) || 'Das Feld muss eine Zahl enthalten',
     (v) =>
       (1999 < v && v < 3001) ||
       'Das Feld muss eine vierstellige Jahrzahl enthalten.',
